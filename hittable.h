a6aecd0c02e6ff285846afcc9fe4b7b9ece8fc0e
Jose <antoniusjose0126@gmail.com>
2026-06-18 06:27:51 +0700

    added: Motion blur, Bounding volume hierarchies

--- hittable.h
+++ hittable.h
@@ -1,6 +1,6 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
-
+#include "aabb.h"
 // #include "ray.h"
 class material;
 
@@ -23,6 +23,8 @@ class hittable {
     virtual ~hittable() = default;
 
     virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
+
+    virtual aabb bounding_box() const = 0;
 };
 
 #endif
